dcf1856c130fe97024f2366d1277191e11369a1b
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-03-27 18:07:39 +0530

    Create Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import { useTheme } from "../hooks/ThemeProvider.jsx";
+
+const Footer = () => {
+  const { currentTheme } = useTheme();
+
+  return (
+    <>
+      <footer className="mt-auto">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center flex justify-center">
+          <span className="text-sm text-black text-center dark:text-white duration-500">
+            Made with {currentTheme === 'dark' ? '🤍' : '🖤'} by
+          </span>
+          <span className="text-sm text-black font-extrabold text-center dark:text-white duration-500">
+            &nbsp;Nureka Rodrigo
+          </span>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
