afc34176733013b4cad076ee9a55ba345adb9a8e
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-03-28 19:09:38 +0530

    Add transition animation

--- src/pages/TvShow.jsx
+++ src/pages/TvShow.jsx
@@ -73,7 +73,7 @@ const TvShow = () => {
                 <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} alt="working on housing" width={1300}
                      className="w-full md:h-full object-cover rounded-lg"/>
               </div>
-              <div className="md:w-1/2 space-y-12 text-gray-700 dark:text-gray-300">
+              <div className="md:w-1/2 space-y-12 text-gray-700 dark:text-gray-300 transition duration-500">
                 <h1
                   className="h-12 text-transparent bg-clip-text bg-gradient-to-br from-blue-600 from-20% via-indigo-400 via-30% to-teal-600 font-semibold text-2xl sm:text-3xl md:text-4xl">
                   {data.name}
